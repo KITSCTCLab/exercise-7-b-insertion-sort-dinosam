5c955ea178b7310a317eda49d056dee6d9210c1c
DINO <109952555+dinosam@users.noreply.github.com>
2022-09-13 18:01:53 +0530

    2nd commit

--- Main.py
+++ Main.py
@@ -1,7 +1,16 @@
 from typing import List
 
 def insertionSort(array) -> List[int]:
-  for i in range(1,len()
+  # Write your code here
+  for i in range(1, len(array)):
+    key = array[i]
+    j = i - 1
+    while j>=0 and key < array[j]:
+      array[j+1] = array[j]
+      j -= 1
+    array[j+1] = key
+    
+  return array
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
